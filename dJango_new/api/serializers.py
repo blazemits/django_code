77f554fda83fe9054887c9f27cee99cc15187674
jerinpg123 <jerinpg123@gmail.com.com>
2016-04-30 17:13:06 +0530

    'LoginAndSignUpCompleted'

--- dJango_new/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from api.models import Task
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Task
-        fields=('completed','title','description')
--- dJango_new/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-from api.views import test,task_detail,task_list
-
-urlpatterns = patterns('',
-    url(r'^test/$', test),
-    url(r'^tasks/([a-zA-Z]+)$',task_list,name='task_list'),
-    url(r'^tasks/(?P<pk>[0-9]+)$',task_detail,name='task_detail'),
-    )
